c62a3be04cc1a19d6e010c97e980197864cc7722
nataliChavez <science_tec12@hotmail.com>
2019-09-07 18:25:58 -0500

    Trabajando en pantalla Iniciar

--- app/src/main/java/com/example/morpheus/proyectohackathon/LoginActivity.java
+++ app/src/main/java/com/example/morpheus/proyectohackathon/LoginActivity.java
@@ -3,8 +3,11 @@ package com.example.morpheus.proyectohackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageSwitcher;
 
 public class LoginActivity extends AppCompatActivity {
+    private ImageSwitcher imageSwitcher;
+    //private int [] slider = {R.};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
