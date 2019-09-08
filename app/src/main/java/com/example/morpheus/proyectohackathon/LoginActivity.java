00a0d0feda98bcf7647fbc4c2ed0fe1326412c1e
nataliChavez <science_tec12@hotmail.com>
2019-09-08 10:16:13 -0500

    Lista coordenadas cajeros

--- app/src/main/java/com/example/morpheus/proyectohackathon/LoginActivity.java
+++ app/src/main/java/com/example/morpheus/proyectohackathon/LoginActivity.java
@@ -98,13 +98,17 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
                 startActivity(intent);
                 break;
 
-            case R.id.btnIniciarSesion:
+            /*case R.id.btnIniciarSesion:
 
                 FragmentManager fragmentMang = getSupportFragmentManager();
                 Fragment   fragmento =  Camara.getInstance("login");
                 fragmentMang.beginTransaction().replace(R.id.contentLogin, fragmento).commit();
 
-                break;
+                break;*/
+
+            case R.id.btnIniciarSesion:
+                intent = new Intent(LoginActivity.this,PantallaPrincipalActivity.class);
+                startActivity(intent);
 
         }
     }
--- app/src/main/java/com/example/morpheus/proyectohackathon/PantallaPrincipalActivity.java
+++ app/src/main/java/com/example/morpheus/proyectohackathon/PantallaPrincipalActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
@@ -13,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.example.morpheus.proyectohackathon.Fragments.localizacionFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class PantallaPrincipalActivity extends AppCompatActivity
@@ -55,7 +57,22 @@ public class PantallaPrincipalActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.nav_localizacion:
+                fragment = new localizacionFragment();
+                break;
+        }
+
+        if(fragment != null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.contentPrincial,fragment).addToBackStack(null).commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
