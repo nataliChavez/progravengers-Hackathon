f7deaf87b2b861dae3e86fd39962ab3ff3ea4b1f
SaFabi <ti13110050@itess.edu.mx>
2019-09-07 19:55:47 -0500

    Se trabaja en la parte de registro de clientes

--- app/src/main/java/com/example/morpheus/proyectohackathon/LoginActivity.java
+++ app/src/main/java/com/example/morpheus/proyectohackathon/LoginActivity.java
@@ -3,15 +3,33 @@ package com.example.morpheus.proyectohackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageSwitcher;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageSwitcher imageSwitcher;
+    private Button btnRegistrarse;
     //private int [] slider = {R.};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnRegistrarse = findViewById(R.id.btnRegistrarse);
+
+        btnRegistrarse.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnRegistrarse:
+
+                break;
+
+
+        }
+
     }
 }
